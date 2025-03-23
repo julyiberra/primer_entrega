d3cbf2eb7ff9fbd75b052278db3f759c37c19b72
Julieta Iberra <julyiberra@gmail.com>
2025-03-23 01:41:04 -0300

    Se modifica el juego para no acceder a 3 listas diferentes

--- questions.py
+++ questions.py
@@ -24,15 +24,15 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
-# El usuario deberá contestar 3 preguntas
+# Se inicia el puntaje del juego
 score=0
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+# El usuario deberá contestar 3 preguntas aleatorias
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 
+for k,elem in enumerate(questions_to_ask):
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(questions[k])
+    for i, answer in enumerate(answers[k]):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -42,7 +42,7 @@ for _ in range(3):
         if user_answer.isdigit() and int(user_answer)-1 in range(4):
             user_answer=int(user_answer)-1
             # Se verifica si la respuesta es correcta
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == correct_answers_index[k]:
                 print("¡Correcto!")
                 score+=1
                 break
@@ -55,7 +55,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(answers[k][correct_answers_index[k]])
 
     # Se imprime un blanco al final de la pregunta
     print()
